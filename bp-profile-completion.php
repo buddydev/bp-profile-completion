3f6c277d532f84679685170058accb966bf40795
raviousprime <ravivats89@gmail.com>
2020-06-20 02:44:32 +0530

    version update

--- bp-profile-completion.php
+++ bp-profile-completion.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: BuddyPress Profile Completion
- * Version: 1.0.5
+ * Version: 1.0.6
  * Plugin URI: https://buddydev.com/introducing-buddypress-user-profile-completion/
  * Description: Force users to complete required fields, photos and cover.
  * Author: BuddyDev
@@ -41,7 +41,7 @@ if ( ! class_exists( 'BP_Profile_Completion' ) ) {
 		 *
 		 * @var string
 		 */
-		private $version = '1.0.5';
+		private $version = '1.0.6';
 
 		/**
 		 * Class instance
